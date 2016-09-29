d0e169179bc4ede8a52a505847c74ffb6372d6e8
tang7526 <p76031226@mail.ncku.edu.tw>
2016-09-30 00:42:47 +0800

    reduce data structure

--- phonebook_opt.h
+++ phonebook_opt.h
@@ -2,12 +2,9 @@
 #define _PHONEBOOK_H
 
 #define MAX_LAST_NAME_SIZE 16
+#define OPT 1
 
-/* TODO: After modifying the original version, uncomment the following
- * line to set OPT properly */
-// #define OPT 1
-typedef struct __PHONE_BOOK_ENTRY {
-    char lastName[MAX_LAST_NAME_SIZE];
+typedef struct __DETAIL {
     char firstName[16];
     char email[16];
     char phone[10];
@@ -17,6 +14,11 @@ typedef struct __PHONE_BOOK_ENTRY {
     char city[16];
     char state[2];
     char zip[5];
+} detail;
+
+typedef struct __PHONE_BOOK_ENTRY {
+    char lastName[MAX_LAST_NAME_SIZE];
+    struct __DETAIL *pDetail;
     struct __PHONE_BOOK_ENTRY *pNext;
 } entry;
 
